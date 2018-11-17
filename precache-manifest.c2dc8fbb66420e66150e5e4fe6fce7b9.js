06fd1d784d3f0d051f1b258604e9242eac761d65
Charlie Wynn <charliewynn@gmail.com>
2018-11-17 13:23:46 -0600

    Updates

--- precache-manifest.c2dc8fbb66420e66150e5e4fe6fce7b9.js
+++ precache-manifest.c2dc8fbb66420e66150e5e4fe6fce7b9.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/mema-football/static/js/runtime~main.47584f5d.js"
   },
   {
-    "revision": "2147c8ff6feee74afac4",
-    "url": "/mema-football/static/js/main.2147c8ff.chunk.js"
+    "revision": "50dd55ec4d5e2f124225",
+    "url": "/mema-football/static/js/main.50dd55ec.chunk.js"
   },
   {
     "revision": "0cd6058fc3c24877ba89",
     "url": "/mema-football/static/js/1.0cd6058f.chunk.js"
   },
   {
-    "revision": "2147c8ff6feee74afac4",
+    "revision": "50dd55ec4d5e2f124225",
     "url": "/mema-football/static/css/main.7d3a54a3.chunk.css"
   },
   {
-    "revision": "dbaf2a5b6369a55b13896f50bd700755",
+    "revision": "01e9cbadfc3cc6a3f68b697a07e66a14",
     "url": "/mema-football/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/mema-football/precache-manifest.237f3d4d2eb6efb1883082efc8635b3e.js"
+  "/mema-football/precache-manifest.c2dc8fbb66420e66150e5e4fe6fce7b9.js"
 );
 
 workbox.clientsClaim();
