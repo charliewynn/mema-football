9f0e4848bfa12371aaf5ff4e29e1b0e6b04ccc19
Charlie Wynn <charliewynn@gmail.com>
2018-11-17 19:09:04 -0600

    Updates

--- precache-manifest.120516ef3c3b188df076e9c231aa1767.js
+++ precache-manifest.120516ef3c3b188df076e9c231aa1767.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/mema-football/static/js/runtime~main.47584f5d.js"
   },
   {
-    "revision": "4798dc22fd54ce891098",
-    "url": "/mema-football/static/js/main.4798dc22.chunk.js"
+    "revision": "4aecfce0c1d0ff9eb309",
+    "url": "/mema-football/static/js/main.4aecfce0.chunk.js"
   },
   {
     "revision": "0cd6058fc3c24877ba89",
     "url": "/mema-football/static/js/1.0cd6058f.chunk.js"
   },
   {
-    "revision": "4798dc22fd54ce891098",
+    "revision": "4aecfce0c1d0ff9eb309",
     "url": "/mema-football/static/css/main.7d3a54a3.chunk.css"
   },
   {
-    "revision": "c64708d424f8446dec7777b2f1112ca5",
+    "revision": "e773e13e91f91c2ed2d7223938d501d6",
     "url": "/mema-football/index.html"
   }
 ];
